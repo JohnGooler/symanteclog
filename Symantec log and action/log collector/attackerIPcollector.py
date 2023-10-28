9937b949dc760e8ca66f3c9a27f8b55074335e6c
ITman <nimamhd@gmail.com>
2023-10-28 16:08:34 +0330

    Update attackerIPcollector.py

--- Symantec log and action/log collector/attackerIPcollector.py
+++ Symantec log and action/log collector/attackerIPcollector.py
@@ -1,4 +1,6 @@
 import os, subprocess, mysql.connector, time
+from persiantools.jdatetime import JalaliDate
+from datetime import datetime
 
 # get the dir path of this file
 dir_path = os.path.dirname(os.path.realpath(__file__))
@@ -17,6 +19,8 @@ with open(dir_path + "\\config\\config.conf") as conf:
             database = configline[1]
         elif configline[0] == 'refresh time':
             refreshtime = int(configline[1])
+        elif configline[0] == 'Delete_Old_Ips_Interval':
+            Delete_Old_Ips_Interval = int(configline[1])
 
 # create directory if not exist
 dirName = 'symanteclog'
@@ -38,59 +42,76 @@ def countdown(t):
         t -= 1
     print('continue...')
 
-
-# push to database fucntion
-def pushdb(val):
+def DB_Connect(query, value, method):
     try:
-        conndb = mysql.connector.connect(
+        if method == 'push':
+            conndb = mysql.connector.connect(
             host=host,
             user=user,
             passwd=passwd,
             database=database,
-            # auth_plugin='mysql_native_password',
-        )
+            #auth_plugin='mysql_native_password',
+            )
 
-        mycursor = conndb.cursor()
+            mycursor = conndb.cursor()
+            mycursor.executemany(query, value)
+            conndb.commit()
+            
+            #print inserted record
+            if mycursor.rowcount > 0:
+                print(mycursor.rowcount, "record inserted.")
+            
+            else:
+                print('No New Record')
 
-        sql = "INSERT INTO ip_details (attackerip, addedtofirewall) VALUES (%s, 'no')"
+        elif method == 'pull':
+            conndb = mysql.connector.connect(
+            host=host,
+            user=user,
+            passwd=passwd,
+            database=database,
+            #auth_plugin='mysql_native_password',
+            )
 
-        mycursor.executemany(sql, val)
-        conndb.commit()
+            mycursor = conndb.cursor()
+            mycursor.execute(query)
+            result = mycursor.fetchall()
+            return result
+        
+        elif method == 'del':
+            conndb = mysql.connector.connect(
+            host=host,
+            user=user,
+            passwd=passwd,
+            database=database,
+            #auth_plugin='mysql_native_password',
+            )
 
-        if mycursor.rowcount > 0:
-            print(mycursor.rowcount, "record inserted.")
+            mycursor = conndb.cursor()
+            mycursor.execute(query)
+            conndb.commit()
+            print(mycursor.rowcount, "record(s) deleted")
+
         else:
-            print('No New Record')
-
-    except mysql.connector.Error as err:
-        print("Something went wrong: {}".format(err))    
-
-# pull from database function
-def pulldb(query):
-    try:
-        conndb = mysql.connector.connect(
-        host=host,
-        user=user,
-        passwd=passwd,
-        database=database,
-        # auth_plugin='mysql_native_password',
-        )
-
-        mycursor = conndb.cursor()
-        mycursor.execute(query)
-        result = mycursor.fetchall()
-        return result
-    except mysql.connector.Error as err:
-        print("Something went wrong: {}".format(err))
-
+            raise('Method is not Correct')
+    
+    except Exception as err:
+        print("Something went wrong: {}".format(err)) 
 
 # remove duplicate ips
-def remove_dub(newdata, olddata):
+def remove_dub(newdata, olddata):    
     seen = []
+    #convert oldips to sigle array for easy compare
+    oldips = [item for olddata in olddata for item in olddata]
     for i in newdata:
-        if i not in seen:
-            if i not in olddata:
-                seen.append(i)
+        try:
+            #Check one by one IPs
+            if i[0] not in [x[0] for x in seen]:
+                if i[0] not in oldips:
+                    seen.append(i)
+        except Exception as e:
+            print(e)
+
     seen = list(seen)
     return seen
 
@@ -106,16 +127,15 @@ def main():
         ############## end of symantec log collector################################################
 
         try:
-            old_ip = pulldb("SELECT attackerip FROM attackers.ip_details")
+            old_ip = DB_Connect("SELECT attackerip FROM attackers.ip_details", None, 'pull')
             old_ip = [list(x) for x in old_ip]
         except:
             print('Make Sure The database are connected')
 
         # create an empty list for all ip address
-        usable_log = []
+        Ip_of_attacher = []
 
         # Check Symanteclog if exist
-
         line_number = sum(1 for line in open(dir_path + "\\symanteclog\\symantecsec.log", "r"))
 
         try:
@@ -123,31 +143,71 @@ def main():
         except Exception as e:
             print(e)
 
-        for line in range(line_number):
+        for _ in range(line_number):
             logs = f.readline().split('\t')
 
-            # Get only IP address from Log
-
-            index_number = [6]
+            # Get IP-address and date of attack from Log
+            Ip_index_number = [6]
+            date_index_number = [1]
             try:
-                filterd = [logs[val] for val in index_number]
-                usable_log.append(filterd)
+                #Get ip address from log file
+                filterd_ip = [logs[ip] for ip in Ip_index_number]
+                Ip_of_attacher.append(filterd_ip)
 
-            except IndexError:
+                #need a proper date format for database, then convert persian date to gregorian date
+                try:
+                    filterd_tarikh = [logs[tarikh][:10] for tarikh in date_index_number]
+                    filterd_tarikh = filterd_tarikh[0].split('/')
+                    filterd_tarikh = [int(x) for x in filterd_tarikh]
+                    #If Jalali date detected, we will convert it to gregorian
+                    if filterd_tarikh[2] < 2000:
+                        filterd_tarikh = [JalaliDate(int(filterd_tarikh[2]), int(filterd_tarikh[1]), int(filterd_tarikh[0])).to_gregorian().strftime("%Y-%m-%d")]
+                    
+                    else: # Gregorian date type detected
+                        #create a list for mysql data import
+                        filterd_tarikh = ['-'.join(str(i) for i in filterd_tarikh)]
+                        #Convert date to "%m/%d/%Y" format
+                        filterd_tarikh = [datetime.strptime(filterd_tarikh[0], "%m-%d-%Y").strftime("%Y-%m-%d")]
+
+                except ValueError:
+                    print(f"Correct date format is: mm/dd/yyyy \nYour date format {filterd_tarikh[0]} is not correct.")
+                    quit()
+
+                Ip_of_attacher.append(filterd_tarikh)
+            except IndexError as e:
+                print(e)
                 pass
 
-        try:
-            usable_log = remove_dub(usable_log, old_ip)
+        #Prepare ips and date for mysql. Only get ips that attacks today in log
+        FinalIPS = []
+        for i in range(0,len(Ip_of_attacher)-1, 2):
+            try:
+                IPAndDate = Ip_of_attacher[i]+Ip_of_attacher[i+1]
+                if IPAndDate[1] == datetime.now().strftime("%Y-%m-%d"):
+                    FinalIPS.append(Ip_of_attacher[i]+Ip_of_attacher[i+1])
+                else:
+                    #OutDated logs
+                    pass
+            except IndexError:
+                #If list is empty, mean it is starting
+                print(IndexError)
+                pass
 
+        #remove duplicated IPS
+        try:
+            Ip_of_attacher = remove_dub(FinalIPS, old_ip)
+            
         except:
             print('Make Sure the files are exist')
-
+        
         # write ips to database
-        pushdb(usable_log)
+        DB_Connect("INSERT INTO ip_details (attackerip, createdat, addedtofirewall) VALUES (%s, %s, 'no')", Ip_of_attacher, 'push')
 
-        # wait 60 seconds
+        # wait in seconds
         countdown(refreshtime)
 
+        #Remove old data from Database. Configurable from config file
+        DB_Connect(f"Delete FROM ip_details WHERE createdat < NOW() - INTERVAL {Delete_Old_Ips_Interval} DAY", None,  method="del")
 
 if __name__ == "__main__":
     main()
